43b5f1557e94f87f8916bf4b68116dab0dc123ca
Yamila Levalle <38963831+ylevalle@users.noreply.github.com>
2020-03-13 16:06:43 -0300

    First Version

---
+++ src/ridge_freq_2.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+from frequest import frequest
+
+def rifdge_freq(im, mask, orient, blksze, windsze,minWaveLength, maxWaveLength):
+    rows,cols = im.shape;
+    freq = np.zeros((rows,cols));
+    
+    for r in range(0,rows-blksze,blksze):
+        for c in range(0,cols-blksze,blksze):
+            blkim = im[r:r+blksze][:,c:c+blksze];
+            blkor = orient[r:r+blksze][:,c:c+blksze];
+            
+            
+            freq[r:r+blksze][:,c:c+blksze] = frequest(blkim,blkor,windsze,minWaveLength,maxWaveLength);
+    
+    freq = freq*mask;
+    freq_1d = np.reshape(freq,(1,rows*cols));
+    ind = np.where(freq_1d>0);
+    
+    ind = np.array(ind);
+    ind = ind[1,:];    
+    
+    non_zero_elems_in_freq = freq_1d[0][ind];    
+    
+    medianfreq = np.median(non_zero_elems_in_freq);
+    
+    return(medianfreq)
